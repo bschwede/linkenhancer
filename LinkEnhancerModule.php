c88d3376a40a48f5218267d004f1d3c0cb3436e3
Bernd <bernd.schwendinger@gmx.de>
2025-08-03 14:40:16 +0200

    wthb link insert - less flickering than with initJs variant

--- LinkEnhancerModule.php
+++ LinkEnhancerModule.php
@@ -411,9 +411,39 @@ class LinkEnhancerModule extends AbstractModule implements ModuleCustomInterface
             $wiki_url = $this->getPref(self::PREF_GENWIKI_LINK, self::STDLINK_GENWIKI);
             $help_title = str_starts_with($help_url, $wiki_url) ? I18N::translate('Webtrees Manual') : /*I18N: webtrees.pot */ I18N::translate('Help');
             
-            $initJs .= "jQuery('ul.wt-user-menu, ul.nav.small').prepend('<li class=\"nav-item menu-wthb\"><a class=\"nav-link\" href=\"" 
-                . e($help_url)
-                . "\"><i class=\"fa-solid fa-circle-question\"></i> {$help_title}</a></li>');";
+            #$initJs .= "jQuery('ul.wt-user-menu, ul.nav.small').prepend('<li class=\"nav-item menu-wthb\"><a class=\"nav-link\" href=\"" 
+            #    . e($help_url)
+            #    . "\"><i class=\"fa-solid fa-circle-question\"></i> {$help_title}</a></li>');";
+            // less flickering than with initJs variant
+            $help_url_e = e($help_url);
+            $includeRes .= <<<EOT
+<script>(() => {
+    const fnwtlink = (node) => {
+        if (document.querySelector('li.nav-item.menu-wthb')) return;
+        const topmenu = node ?? document.querySelector('ul.wt-user-menu, ul.nav.small');
+        if (!topmenu) return;
+        topmenu.insertAdjacentHTML('afterbegin', "<li class=\"nav-item menu-wthb\"><a class=\"nav-link\" href=\"$help_url_e\"><i class=\"fa-solid fa-circle-question\"></i> $help_title</a></li>");
+    };
+    const callback = function (mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(node => {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        if (node.tagName === 'UL' && (node.classList.contains('wt-user-menu') || (node.classList.contains('nav') && node.classList.contains('small')))) {
+                            fnwtlink(node);
+                        }
+                    }
+                });
+            }
+        }
+    };
+
+    fnwtlink();
+    const observer = new MutationObserver(callback);
+    observer.observe(document, { childList: true, subtree: true });
+})()</script>
+EOT;
+            
         }        
 
         // === admin backend - only if patch P002 for administration.phtml was applied; default: headContent of custom modules is not called on the admin backend
@@ -426,7 +456,8 @@ class LinkEnhancerModule extends AbstractModule implements ModuleCustomInterface
             || (str_starts_with($activeRouteInfo['path'], '/module') && str_starts_with($action, 'admin'))
         ) {
                 if ($cfg_wthb_active) {
-                    return $this->getInitJavascript($initJs);
+                    //return $this->getInitJavascript($initJs);
+                    return $includeRes . ($initJs ? $this->getInitJavascript($initJs) : '');
                 }
                 return ''; # other stuff is of no use in admin backend
         }
