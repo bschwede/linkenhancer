6c498a5623bda831a5b906d419b024db9885ee66
Bernd <bernd.schwendinger@gmx.de>
2025-08-14 20:17:29 +0200

    home link css routine switched from js to php

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -92,8 +92,7 @@ const jsMde = () => jsPipe("./resources/js/tiny-mde-wt.js", 'mde');
 const jsMdeLe = () => jsPipe("./resources/js/index-le-mde.js", 'le-mde');
 const jsLe = () => jsPipe("./resources/js/linkenhancer.js", 'le');
 const jsWTHBLink = () => jsPipe("./resources/js/wthb-link.js", 'wthb-link', 'cjs');
-const jsHomeLink = () => jsPipe("./resources/js/home-link.js", 'home-link', 'cjs');
-const jscripts = gulp.series(jsMde, jsMdeLe, jsLe, jsExtractLeConfig, jsWTHBLink, jsHomeLink);
+const jscripts = gulp.series(jsMde, jsMdeLe, jsLe, jsExtractLeConfig, jsWTHBLink);
 
 //--- CSS
 const cssPipe = (inputFile, outputInfix) => 
