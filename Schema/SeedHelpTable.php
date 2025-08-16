80068b987a933eaa663a77f3036b3e86287c915e
Bernd <bernd.schwendinger@gmx.de>
2025-08-16 12:30:29 +0200

    documentation

--- Schema/SeedHelpTable.php
+++ Schema/SeedHelpTable.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ * webtrees - linkenhancer (custom module)
+ *
+ * Copyright (C) 2025 Bernd Schwendinger
+ *
+ * webtrees: online genealogy application
+ * Copyright (C) 2025 webtrees development team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses/>.
+ */
 declare(strict_types=1);
 
 namespace Schwendinger\Webtrees\Module\LinkEnhancer\Schema;
@@ -7,9 +28,9 @@ use Fisharebest\Webtrees\DB;
 use Fisharebest\Webtrees\Schema\SeedInterface;
 
 /**
- * Populate the default_resn table
+ * Populate the route_hel_map table
  */
-class SeedHelpTable //implements SeedInterface
+class SeedHelpTable implements SeedInterface
 {
     private const array HEADER = ['path', 'handler', 'method', 'extras', 'category', 'order', 'url'];
 
