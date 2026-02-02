c885d56ad32735b0a501c7150ce066a2e2f10b5c
Bernd <bernd.schwendinger@gmx.de>
2026-02-02 08:36:00 +0100

    copyright prologue

--- CommonMark/LeTableOfContentsRenderer.php
+++ CommonMark/LeTableOfContentsRenderer.php
@@ -3,12 +3,33 @@
 declare(strict_types=1);
 
 /*
+ * base code taken from: vendor/league/commonmark/src/Extension/TableOfContents/TableOfContentsRenderer.php
  * This file is part of the league/commonmark package.
  *
  * (c) Colin O'Dell <colinodell@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+------
+ * webtrees - linkenhancer (custom module)
+ *
+ * Copyright (C) 2026 Bernd Schwendinger
+ *
+ * webtrees: online genealogy application
+ * Copyright (C) 2026 webtrees development team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses/>. 
  */
 
 //namespace League\CommonMark\Extension\TableOfContents;
