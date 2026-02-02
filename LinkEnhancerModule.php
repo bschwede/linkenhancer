8f3cc2ab01b68c8eec00511fbe2cb43801d6a923
Bernd <bernd.schwendinger@gmx.de>
2026-02-02 09:24:18 +0100

    v1.2.4

--- LinkEnhancerModule.php
+++ LinkEnhancerModule.php
@@ -72,7 +72,7 @@ class LinkEnhancerModule extends AbstractModule implements ModuleCustomInterface
     public const CUSTOM_AUTHOR = 'Bernd Schwendinger';
     public const GITHUB_USER = 'bschwede';
     public const CUSTOM_WEBSITE = 'https://github.com/' . self::GITHUB_USER . '/' . self::CUSTOM_MODULE . '/';
-    public const CUSTOM_VERSION = '1.2.3';
+    public const CUSTOM_VERSION = '1.2.4';
     public const CUSTOM_LAST = 'https://raw.githubusercontent.com/' . self::GITHUB_USER . '/' .
         self::CUSTOM_MODULE . '/main/latest-version.txt';
 
