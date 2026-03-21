de022e268dd9460a9bdb168b0e9efb84192b9dd4
Bernd <bernd.schwendinger@gmx.de>
2026-03-21 13:23:35 +0100

    support multiple entries per key; fix for missing text replacement (getNextLink)

--- resources/js/src/le-link-processor.js
+++ resources/js/src/le-link-processor.js
@@ -32,132 +32,135 @@ export const processLinks = (
         const params = new URLSearchParams(hash);
 
         const matchingKeys =
-            Object.keys(LEtargets)
+            Object.keys(LEtargets) // important to match against key order of target config, where wt-key is the first; only works, if there are no nnumeric keys!
                 .filter(k => params.has(k));
 
-        const unknownKeys =
-            [...params.keys()]
-                .filter(k => !matchingKeys.includes(k));
+        const unknownKeys = 
+            [...new Set(
+                [...params.keys()]
+                    .filter(k => !matchingKeys.includes(k))
+            )].sort(); // list unknown parameter keys once in lexicographical order
 
         let lastLink = null;
         const diatitle = $("a.dropdown-item.menu-chart-tree[role=menuitem]").text().trim() || LEoptions.I18N['Interactive tree'];
 
         matchingKeys.forEach(key => {
 
-            const option = params.get(key);
+            const options = params.getAll(key); // support multiple entries per key
 
-            if (!option) return;
+            options.forEach(option => {
+                if (!option) return;
 
-            if (key === 'wt') {
+                if (key === 'wt') {
 
-                const parsed =
-                    parseCrossReferenceLink(option, rectypes);
+                    const parsed =
+                        parseCrossReferenceLink(option, rectypes);
 
-                if (!parsed) return;
+                    if (!parsed) return;
 
-                let { type, xref, newtree, dia } = parsed;
-                newtree = (newtree === LEoptions.tree ? null : newtree);
+                    let { type, xref, newtree, dia } = parsed;
+                    newtree = (newtree === LEoptions.tree ? null : newtree);
 
-                if ((!type && type !== '') || !xref) {
-                    let nextLink = getNextLink(link, lastLink);
-                    lastLink = setLink(
-                        document,
-                        nextLink,
-                        lastLink, 
-                        '',
-                        LEcfg[key].name + " - " + LEoptions.I18N['syntax error'] + "!", 
-                        LEcfg[key].cname,
-                        LEoptions
-                    );
+                    if ((!type && type !== '') || !xref) {
+                        let nextLink = lastLink ? createLink(document) : link;
+                        lastLink = setLink(
+                            document,
+                            nextLink,
+                            lastLink, 
+                            '',
+                            LEcfg[key].name + " - " + LEoptions.I18N['syntax error'] + "!", 
+                            LEcfg[key].cname,
+                            LEoptions
+                        );
+                        lastLink.classList.add('icon-wt-xref');
+                        return;
+                    }
+
+                    let url = LEoptions.baseurl;
+                    let thisXrefShown = false;
+                    let xrefsuffix = '';                
+
+                    if (newtree) {
+                        url = url.replace(`/tree/${LEoptions.tree}`.replaceAll('/', separator[LEoptions.urlmode].path),
+                            `/tree/${newtree}`.replaceAll('/', separator[LEoptions.urlmode].path));
+                        xrefsuffix = ` @ ${newtree}`;
+                    } else if (LEoptions.thisXref === xref) {
+                        link = changeTagName(document, link, 'mark');
+                        thisXrefShown = true;
+                    }
+
+                    const urlxref =
+                        url +
+                        separator[LEoptions.urlmode].path +
+                        (type !== '' ? rectypes[type] : 'goto-xref') +
+                        separator[LEoptions.urlmode].path +
+                        xref;
+
+                    let nextLink =
+                        lastLink ? createLink(document) : link;
+
+                    lastLink =
+                        setLink(
+                            document,
+                            nextLink,
+                            lastLink,
+                            urlxref,
+                            `${LEtargets[key].name} - ${xref}${xrefsuffix}`,
+                            LEtargets[key].cname,
+                            LEoptions
+                        );
                     lastLink.classList.add('icon-wt-xref');
-                    return;
-                }
 
-                let url = LEoptions.baseurl;
-                let thisXrefShown = false;
-                let xrefsuffix = '';                
-
-                if (newtree) {
-                    url = url.replace(`/tree/${LEoptions.tree}`.replaceAll('/', separator[LEoptions.urlmode].path),
-                        `/tree/${newtree}`.replaceAll('/', separator[LEoptions.urlmode].path));
-                    xrefsuffix = ` @ ${newtree}`;
-                } else if (LEoptions.thisXref === xref) {
-                    link = changeTagName(document, link, 'mark');
-                    thisXrefShown = true;
-                }
-
-                const urlxref =
-                    url +
-                    separator[LEoptions.urlmode].path +
-                    (type !== '' ? rectypes[type] : 'goto-xref') +
-                    separator[LEoptions.urlmode].path +
-                    xref;
-
-                let nextLink =
-                    lastLink ? createLink(document) : link;
-
-                lastLink =
-                    setLink(
-                        document,
-                        nextLink,
-                        lastLink,
-                        urlxref,
-                        `${LEtargets[key].name} - ${xref}${xrefsuffix}`,
-                        LEtargets[key].cname,
-                        LEoptions
-                    );
-                lastLink.classList.add('icon-wt-xref');
-
-                if (type == 'i' && dia && !thisXrefShown) {
-                    let diaurl = url.replace(
-                            "/tree/".replaceAll('/', separator[LEoptions.urlmode].path),
-                            "/module/tree/Chart/".replaceAll('/', separator[LEoptions.urlmode].path)
-                        ) + separator[LEoptions.urlmode].option + "xref=" + xref;
-                    nextLink = lastLink ? createLink(document) : link;
-                    lastLink = setLink(
-                        document,
-                        nextLink,
-                        lastLink,
-                        diaurl,
-                        `${diatitle} - ${xref}`,
-                        'icon-wt-dia',
-                        LEoptions
-                    );
-                }
-            } else {
-
-                let url = '';
-                let title = LEtargets[key].name;
-
-                if (typeof LEtargets[key].url === 'function') {
-
-                    ({ url, title } =
-                        LEtargets[key].url(option, title));
-
+                    if (type == 'i' && dia && !thisXrefShown) {
+                        let diaurl = url.replace(
+                                "/tree/".replaceAll('/', separator[LEoptions.urlmode].path),
+                                "/module/tree/Chart/".replaceAll('/', separator[LEoptions.urlmode].path)
+                            ) + separator[LEoptions.urlmode].option + "xref=" + xref;
+                        nextLink = lastLink ? createLink(document) : link;
+                        lastLink = setLink(
+                            document,
+                            nextLink,
+                            lastLink,
+                            diaurl,
+                            `${diatitle} - ${xref}`,
+                            'icon-wt-dia',
+                            LEoptions
+                        );
+                    }              
                 } else {
 
-                    url =
-                        LEtargets[key].url +
-                        encodeURIComponent(option);
+                    let url = '';
+                    let title = LEtargets[key].name;
+
+                    if (typeof LEtargets[key].url === 'function') {
+
+                        ({ url, title } =
+                            LEtargets[key].url(option, title));
+
+                    } else {
+
+                        url =
+                            LEtargets[key].url +
+                            encodeURIComponent(option);
+                    }
+
+                    const nextLink =
+                        lastLink ? createLink(document) : link;
+
+                    title = title.replace(/\$ID/ig, decodeURIComponent(option));
+
+                    lastLink =
+                        setLink(
+                            document,
+                            nextLink,
+                            lastLink,
+                            url,
+                            title,
+                            LEtargets[key].cname,
+                            LEoptions
+                        );
                 }
-
-                const nextLink =
-                    lastLink ? createLink(document) : link;
-
-                title = title.replace(/\$ID/ig, decodeURIComponent(option));
-
-                lastLink =
-                    setLink(
-                        document,
-                        nextLink,
-                        lastLink,
-                        url,
-                        title,
-                        LEtargets[key].cname,
-                        LEoptions
-                    );
-            }
-
+            });
         });
 
 
