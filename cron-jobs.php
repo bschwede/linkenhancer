63dc513cd5ec86ac429587b433ca2d0c5a1ec8cb
Bernd <bernd.schwendinger@gmx.de>
2026-09-19 10:32:05 +0200

    command parameter modified

--- cron-jobs.php
+++ cron-jobs.php
@@ -38,7 +38,7 @@ return [
             'description' => MoreI18N::translate('Rebuild the link index incrementally.'),
             'params' => [
                 ['name' => '--limit', 'optional' => true, 'default' => '5000', 'description' => MoreI18N::translate('max records per run')],
-                ['name' => '--tree', 'optional' => true, 'description' => MoreI18N::translate('id of the tree to be scanned - all if not set')],
+                ['name' => '--tree=ID', 'optional' => true, 'description' => MoreI18N::translate('number of the tree to be scanned - all if not set')],
                 ['name' => '--rebuild', 'optional' => true, 'description' => MoreI18N::translate('force a full rebuild')],
                 ['name' => '--flush', 'optional' => true, 'description' => MoreI18N::translate('empty the UID index and exit')],
             ],
@@ -48,7 +48,7 @@ return [
             'description' => MoreI18N::translate('Rebuild the UID index incrementally.'),
             'params' => [
                 ['name' => '--limit', 'optional' => true, 'default' => '5000', 'description' => MoreI18N::translate('max records per run')],
-                ['name' => '--tree', 'optional' => true, 'description' => MoreI18N::translate('id of the tree to be scanned - all if not set')],
+                ['name' => '--tree=ID', 'optional' => true, 'description' => MoreI18N::translate('number of the tree to be scanned - all if not set')],
                 ['name' => '--rebuild', 'optional' => true, 'description' => MoreI18N::translate('force a full rebuild')],
                 ['name' => '--flush', 'optional' => true, 'description' => MoreI18N::translate('empty the UID index and exit')],
             ],
